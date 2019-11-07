4d51e76ecde55d66406af4a7dae6c3f405480eca
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-07 13:05:44 -0800

    Configure Amplify

---
+++ src/graphql/queries.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = `query GetNote($id: ID!) {
+  getNote(id: $id) {
+    id
+    note
+  }
+}
+`;
+export const listNotes = `query ListNotes(
+  $filter: ModelNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      note
+    }
+    nextToken
+  }
+}
+`;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

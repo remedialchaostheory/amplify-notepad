0257b964ea8f86b85112a2dd30a5e747bdb5f1af
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-07 20:03:27 -0800

    Add listing of all notes from GraphQL request

--- src/Notepad.js
+++ src/Notepad.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NoteForm from "./NoteForm";
 import NoteList from "./NoteList";
+import { listNotes } from "./graphql/queries";
+import {API, graphqlOperation} from "aws-amplify";
 
 class Notepad extends Component {
   constructor(props) {
@@ -9,7 +11,15 @@ class Notepad extends Component {
       notes: []
     };
     this.updateNotes = this.updateNotes.bind(this);
+
   }
+
+  async componentDidMount() {
+    const resp = await API.graphql(graphqlOperation(listNotes));
+    const updatedNotes = resp.data.listNotes.items;
+    this.setState({ notes: updatedNotes })
+  }
+
   updateNotes = newNote => {
     this.setState({ notes: [newNote, ...this.state.notes] });
     console.log('newNote ->', newNote);

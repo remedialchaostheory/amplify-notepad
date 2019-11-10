856695e7ec4f49afae113abd3fc7895cefd316f3
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-09 18:36:57 -0800

    Add transition of note creation and deletion

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './NoteList.css';
 
 class NoteList extends Component {
@@ -6,30 +7,36 @@ class NoteList extends Component {
     const strikeOnHoveredCross = noteId => (this.props.isHoveredCross && noteId === this.props.hoveredNote) && "strike";
     return (
         <div className="fl w-100 w-65-ns sans-serif pointer">
-          {this.props.notes.map(note => (
-              <div
-                  key={note.id}
-                  className="flex items-center"
-                  onMouseEnter={() => this.props.handleHoveredNote(note.id)}
-                  onMouseLeave={() => this.props.handleHoveredNote("")}
-              >
-                <li
-                    className={`list pa1 fa3 ${this.props.hoveredNote && "dim"} ${strikeOnHoveredCross(note.id)}`}
-                    onClick={() => this.props.handleEditNote(note)}
+          <ReactCSSTransitionGroup
+              transitionName="fade"
+              transitionEnterTimeout={0}
+              transitionLeaveTimeout={0}
+          >
+            {this.props.notes.map(note => (
+                <div
+                    key={note.id}
+                    className="flex items-center"
+                    onMouseEnter={() => this.props.handleHoveredNote(note.id)}
+                    onMouseLeave={() => this.props.handleHoveredNote("")}
                 >
-                  {note.note}
-                </li>
-                {/* Delete */}
-                <button
-                    className="bg-transparent bn f4 grow-large pointer"
-                    onMouseEnter={this.props.handleHoveredCross}
-                    onMouseLeave={this.props.handleHoveredCross}
-                    onClick={() => this.props.handleDeleteNote(note.id)}
-                >
-                  <span>&times;</span>
-                </button>
-              </div>
-          ))}
+                  <li
+                      className={`list pa1 fa3 ${this.props.hoveredNote && "dim"} ${strikeOnHoveredCross(note.id)}`}
+                      onClick={() => this.props.handleEditNote(note)}
+                  >
+                    {note.note}
+                  </li>
+                  {/* Delete */}
+                  <button
+                      className="bg-transparent bn f4 grow-large pointer"
+                      onMouseEnter={this.props.handleHoveredCross}
+                      onMouseLeave={this.props.handleHoveredCross}
+                      onClick={() => this.props.handleDeleteNote(note.id)}
+                  >
+                    <span>&times;</span>
+                  </button>
+                </div>
+            ))}
+          </ReactCSSTransitionGroup>
         </div>
     )
   }

59b8e257558e1226813c1ccfd5e75e61b4405cc7
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-10 10:42:05 -0800

    Edit default signed in navbar greeting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Notepad from "./Notepad";
 import './App.css';
-import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
+import { withAuthenticator, Authenticator, AmplifyTheme, Greetings } from "aws-amplify-react";
 
 function App() {
   return (
     <div>
+      <Authenticator hideDefault={true}>
+        <Greetings
+            inGreeting={(username) => 'Yo ' + username}
+            outGreeting="Please sign in..."
+        />
+      </Authenticator>
+
       <Notepad />
 
     </div>
   );
 }
 
+// class customGreetings extends
+
 const theme = { ...AmplifyTheme };
 
-export default withAuthenticator(App, true, [], null, theme);
+export default withAuthenticator(App, false, [], null, theme);

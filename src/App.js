241d343d6cb105ccc0f7ddaad20b7fc571011a89
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-27 14:59:09 -0800

    Auto format via Prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 import Notepad from "./Notepad";
-import './App.css';
+import "./App.css";
+// prettier-ignore
 import { withAuthenticator, Authenticator, AmplifyTheme, Greetings } from "aws-amplify-react";
 import { Auth } from "aws-amplify";
 
 function App() {
   let currUser;
   Auth.currentAuthenticatedUser()
-      .then(user => {
-        currUser = user.attributes.email;
-      })
-      .catch(err => console.log(err));
+    .then(user => {
+      currUser = user.attributes.email;
+    })
+    .catch(err => console.log(err));
 
   return (
     <div>
       <Authenticator hideDefault={true}>
         <Greetings
-            inGreeting={() => 'Welcome, ' + currUser}
-            outGreeting="Please sign in..."
+          inGreeting={() => "Welcome, " + currUser}
+          outGreeting="Please sign in..."
         />
       </Authenticator>
 
       <Notepad />
-
     </div>
   );
 }
